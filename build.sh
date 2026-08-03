15bd0c7753f8eb6ffc0520ae2c357e6d9c1c144a
G.SIVA RANI <sivarani657@gmail.com>
2026-08-03 14:26:56 +0530

    Enhance build script with installation and migration steps
    Added steps to install dependencies, collect static files, and apply database migrations.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
